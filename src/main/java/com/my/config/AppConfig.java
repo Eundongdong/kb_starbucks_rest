70c5a2eb8736726613c6cb968cd88b04c387be53
[username] <51512016+Eundongdong@users.noreply.github.com>
2024-08-24 14:43:47 +0900

    build : 기본 spring config 설정
    - config package
    - resources/ log 설정 파일
    - 인코딩 필터 설정시 Servlet 필요 -> gradle import

---
+++ src/main/java/com/my/config/AppConfig.java
@@ -0,0 +1,73 @@
+package com.my.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import javax.sql.DataSource;
+
+@Configuration
+//@ComponentScan(basePackages = "com.multi")
+@ComponentScan(basePackages = {"com.my",
+        "com.my"
+})
+public class AppConfig {
+    public AppConfig() {
+        System.out.println("AppConfig created");
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        config.setJdbcUrl("jdbc:log4jdbc:mysql://localhost:3306/shop?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&characterEncoding=UTF-8&useUnicode=true");
+
+        config.setUsername("root");
+        config.setPassword("1234");
+
+        config.setConnectionTimeout(30000);
+        config.setMinimumIdle(3);
+        config.setMaximumPoolSize(10);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+        config.setAutoCommit(true);
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        org.apache.ibatis.session.Configuration configuration = getConfiguration();
+        sessionFactory.setConfiguration(configuration);
+
+        Resource[] mapperLocations = new Resource[] {
+                //new ClassPathResource("mapper/BoardMapper.xml")
+        };
+        sessionFactory.setMapperLocations(mapperLocations);
+
+        return sessionFactory.getObject();
+    }
+    private org.apache.ibatis.session.Configuration getConfiguration() {
+        org.apache.ibatis.session.Configuration configuration =
+                new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+       return configuration;
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+}
+
+
