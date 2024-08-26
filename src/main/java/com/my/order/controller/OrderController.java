e3fdf681345503b9bf395b2c34d65298b72a73bb
Eundongdong <jk4373@sookmyung.ac.kr>
2024-08-26 14:33:11 +0900

    feat : Order URI 매핑 기능 추가
    OrderController, Service 어노태이션 추가
    
    - TODO : Mapper 파일 변경해야함

--- src/main/java/com/my/order/controller/OrderController.java
+++ src/main/java/com/my/order/controller/OrderController.java
@@ -8,6 +8,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -16,6 +19,7 @@ import java.util.Map;
 
 @Controller
 @Slf4j
+@RequestMapping("/order")
 public class OrderController {
     OrderService orderService;
 
@@ -24,6 +28,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @PostMapping("")
     public ResponseEntity add(HttpSession session){
         //session값가져오기
         String loginedId = (String) session.getAttribute("loginedId");
@@ -31,21 +36,35 @@ public class OrderController {
         if(loginedId == null){
             return ResponseEntity.status(401).build();
         }
-
+        log.info(loginedId);
         Map<String,Integer> cart = (Map)session.getAttribute("cart");
         //카트에 아무것도 없으면
         if (cart == null || cart.isEmpty()) {
             return ResponseEntity.status(400).build();
         }
+        log.info(cart.toString());
         orderService.add(loginedId, cart);
         session.removeAttribute("cart");
+        log.info(cart.toString());
         return ResponseEntity.status(200).build();
 
 
     }
 
-//    public ResponseEntity<List<OrderInfo>> list(HttpSession session){
-//
-//    }
+    /**
+     * 주문 기록 전체 조회
+     * @param session
+     * @return
+     */
+    @GetMapping("")
+    public ResponseEntity<List<OrderInfo>> list(HttpSession session){
+        String loginedId = (String) session.getAttribute("loginedId");
+        if(loginedId == null){
+            return ResponseEntity.status(401).build();
+        }
+        List<OrderInfo> orderInfoList = orderService.list(loginedId);
+        return ResponseEntity.status(200).body(orderInfoList);
+
+    }
 
 }
--- src/main/java/com/my/order/service/OrderServiceImp1.java
+++ src/main/java/com/my/order/service/OrderServiceImp1.java
@@ -4,15 +4,19 @@ import com.my.order.mapper.OrderMapper;
 import com.my.order.vo.OrderInfo;
 import com.my.order.vo.OrderLine;
 import com.my.product.vo.Product;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Service
+@Transactional
+@Slf4j
 public class OrderServiceImp1 implements OrderService {
     private SqlSessionTemplate sqlSession;
 
@@ -26,6 +30,7 @@ public class OrderServiceImp1 implements OrderService {
         OrderInfo info = new OrderInfo();
         info.setOrderId(loginedId);
         List<OrderLine> lines = new ArrayList<>();
+        System.out.println("cart 받아오기 시작");
         for(Map.Entry<String,Integer> e : cart.entrySet()){
             String prodNo = e.getKey();
             Integer quantity = e.getValue();
@@ -37,14 +42,19 @@ public class OrderServiceImp1 implements OrderService {
             lines.add(line);
         }
         info.setLines(lines);
+
+        log.info("info{}",info);
         OrderMapper mapper = sqlSession.getMapper(OrderMapper.class);
         //TODO Transaction으로 관리해야함
         mapper.insert(info);
+        System.out.println("insert 완료");
         return info;
     }
 
     @Override
     public List<OrderInfo> list(String loginedId) {
-        return List.of();
+        OrderMapper mapper = sqlSession.getMapper(OrderMapper.class);
+        List<OrderInfo> orderInfoList = mapper.findByOrderId(loginedId);
+        return orderInfoList;
     }
 }
