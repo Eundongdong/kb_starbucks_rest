6f35200cc900b3e24aa3285122ec8b830b8cc70b
Eundongdong <jk4373@sookmyung.ac.kr>
2024-08-26 17:26:20 +0900

    test : product service 테스트
    - test 디렉토리 추가
    - ProductServiceTest 추가

---
+++ src/test/java/com/my/product/service/ProductServiceTest.java
@@ -0,0 +1,49 @@
+package com.my.product.service;
+
+
+import com.my.product.exception.FindException;
+import com.my.product.vo.Product;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class) //SpringFramework JUnit 실행 가능하도록 설정한다.
+@ContextConfiguration(classes = com.my.config.AppConfig.class)
+public class ProductServiceTest {
+    @Autowired
+    ProductService productService;
+
+    @Test
+    public void testList() throws FindException {
+        List<Product> list = productService.list();
+        assertTrue(list.size()> 0);
+    }
+    @Test
+    public void testDetail() throws FindException {
+        String prodNo = "C0001";
+        String expectedName = "아메리카노";
+        int expectedPrice = 1100;
+        Product p  = productService.detail(prodNo);
+        assertEquals(expectedName, p.getProdName());
+        assertEquals(expectedPrice, p.getProdPrice());
+    }
+
+    /**
+     * 상품상세 예외 발생 테스트
+     */
+    @Test
+    public void testNotFoundDetail() {
+        String prodNo = "없는상품번호";
+        FindException expectedException;
+
+        int expectedPrice = 1100;
+        expectedException = assertThrows(FindException.class,
+                ()->productService.detail(prodNo));
+    }
+}
